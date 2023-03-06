eae7d4daa37f3a7fd4239e588848a01d08cf6bd7
dyoung2012 <108146998+dyoung2012@users.noreply.github.com>
2023-03-06 12:11:53 -0800

    Updated role.tf and eks.tf

--- roles.tf
+++ roles.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "EKSClusterRole" {
-  name = "EKSClusterRole"
+resource "aws_iam_role" "Team3EKSClusterRole" {
+  name = "Team3EKSClusterRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -13,16 +13,16 @@ resource "aws_iam_role" "EKSClusterRole" {
     ]
   })
 }
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "Team3AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.EKSClusterRole.name
+  role       = aws_iam_role.Team3EKSClusterRole.name
 }
-resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+resource "aws_iam_role_policy_attachment" "Team3AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.EKSClusterRole.name
+  role       = aws_iam_role.Team3EKSClusterRole.name
 }
-resource "aws_iam_role" "NodeGroupRole" {
-  name = "EKSNodeGroupRole"
+resource "aws_iam_role" "Team3NodeGroupRole" {
+  name = "Team3EKSNodeGroupRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -37,15 +37,15 @@ resource "aws_iam_role" "NodeGroupRole" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "Team3AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.NodeGroupRole.name
+  role       = aws_iam_role.Team3NodeGroupRole.name
 }
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "Team3AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.NodeGroupRole.name
+  role       = aws_iam_role.Team3NodeGroupRole.name
 }
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "Team3AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.NodeGroupRole.name
+  role       = aws_iam_role.Team3NodeGroupRole.name
 }
